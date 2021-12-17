72185b985ddc1e2419e8bcc82adb2882cc326366
spongebobaa16 <83540155+spongebobaa16@users.noreply.github.com>
2021-12-18 01:58:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// var title = document.getElementById("title");
+// var content = document.getElementById("content");
+// var btn = document.getElementById("btn");
+// var list = document.getElementById("list");
+// var article1 = document.getElementById("article1");
+
+// content.addEventListener("keyup", function(){
+//     article1.innerText = content.value;
+    
+// })
+
+// btn.addEventListener("click", function(){
+//     list.innerHTML = list.innerHTML + `
+//     <div class="article">
+//         <h2>${title.value}</h2>
+//         <p>${content.value}</p>
+//     </div>
+//     `;
+//     title.value="";
+//     content.value="";
+
+// })
+
+
+
+
+// var count = 0;
+
+// function handle_click(element){
+//     // console.log(element)
+//     if(count % 3 == 0)
+//         element.innerText = "wtf"
+//     else if(count % 3 == 1)
+//         element.innerText = "ok";
+//     else
+//         element.innerText = "nice";
+
+//     count ++;
+// }
+
+
+
+
+// var h1 = document.getElementById("header");
+// // console.log(h1)
+// h1.innerText = "????????"
+// h1.style.backgroundColor = "red";
+// h1.style.color = "blue";
+// var link = document.getElementById("link");
+// link.href = "https://amazon.com"
+
+
+// console.log(link)
+
+
+
+
+
+
+
+
+// var my_name = prompt("type your name");
+// document.write("hello" + my_name);
+
